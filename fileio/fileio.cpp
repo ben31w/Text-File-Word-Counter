d478d595d5ee38fc08b9100ddcf30f40909a4a39
Ben Wright <ben31w@gmail.com>
2022-02-18 16:23:21 -0500

    update writetoFile: don't return failure if output filename DNE

--- fileio/fileio.cpp
+++ fileio/fileio.cpp
@@ -32,22 +32,25 @@ void closeFile(std::fstream& myfile) {
  * 			SUCCESS if all data is written and outputfilename closes OK
  * */
 int writetoFile(std::vector<constants::entry>  &entries, const std::string &outputfilename) {
-	// If we cannot open file, return failure.
-	if ( !does_file_exist(outputfilename) ) {
-			return constants::FAIL_FILE_DID_NOT_OPEN;
-		}
 	// If there are no entries in the array, return failure.
 	if (entries.size() == 0) {
 		return constants::FAIL_NO_ARRAY_DATA;
 	}
 
-	// Write to file if it opens successfully.
+	// Open file.
 	std::ofstream fout;
 	fout.open(outputfilename.c_str());
+
+	// If file doesn't open, return failure
+	if (!fout.is_open()) {
+		return constants::FAIL_FILE_DID_NOT_OPEN;
+	}
+
+	// Write to file.
 	for (constants::entry e: entries) {
-//		std::string toWrite = e.word + " " + std::to_string(e.number_occurences) + "\n";
-//		long int size = toWrite.size();
-//		fout.write(toWrite.c_str(), size );
+		std::string toWrite = e.word + " " + std::to_string(e.number_occurences) + "\n";
+		long int size = toWrite.size();
+		fout.write(toWrite.c_str(), size );
 	}
 	fout.close();
 	return constants::SUCCESS;
